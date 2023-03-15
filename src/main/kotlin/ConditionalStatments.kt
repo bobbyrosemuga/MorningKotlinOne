163e01f4be595288dadc3899e29c99fa48f62ec4
bobbyrosemuga <bobbyrose273@gmail.com>
2023-03-15 10:22:18 +0300

    Initial commit

---
+++ src/main/kotlin/ConditionalStatments.kt
@@ -0,0 +1,17 @@
+fun main(args: Array<String>) {
+    // IF STATEMENT
+    var age = 20
+    if(age <10){
+        println("Sorry are underage")
+    }
+    else{
+        println("Welcome to the party")
+    }
+
+    var weight = 100
+    var height = 1.5
+    var bmi = weight/(height*height)
+    if (bmi<=18){
+        println("Underweight")
+    }
+}
